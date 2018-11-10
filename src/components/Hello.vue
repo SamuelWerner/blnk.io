633b5143418586afa40c2485a897a75c1aeb5138
Samuel Werner <hallo@samuelwerner.de>
2018-11-10 14:10:57 +0100

    Heroku Server Test

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -4,7 +4,7 @@
     <div style="outline:none" contenteditable="true"
          class="my-3 bg-white rounded shadow-lg paper">
       <div>
-        <h1 class="display-3">Hello World</h1>
+        <h1 class="display-3">Hello World+</h1>
         <p class="lead">This is the homepage of your vue app</p>
       </div>
     </div>
