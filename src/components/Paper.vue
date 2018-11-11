06f97d81be6950fc5c7ea226ce59739d3d4763b5
Christopher Sang <christopher.sang@iem.thm.de>
2018-11-11 22:47:16 +0100

    Anpassung Menü- und Toolleiste; kleine Anpassungen

--- src/components/Paper.vue
+++ src/components/Paper.vue
@@ -1,9 +1,9 @@
 <template>
   <main class="container" style="height:100%">
     <div class="row">
-      <div class="col-md-8 order-md-1 order-2">
-        <md-menu md-size="small" md-align-trigger >
-          <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
+      <div class="col-md-9 order-md-1 order-2">
+        <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
+          <md-menu md-size="small" md-align-trigger >
             <button md-menu-trigger  type="button" class="btn btn-light">Datei</button>
 
             <md-menu-content class="menu-content">
@@ -18,16 +18,58 @@
               <md-menu-item @click="">Seiteneinrichtung</md-menu-item>
               <md-menu-item @click="">Drucken</md-menu-item>
             </md-menu-content>
+          </md-menu>
+          <md-menu md-size="small" md-align-trigger >
+            <button md-menu-trigger type="button" class="btn btn-light">Bearbeiten</button>
 
-            <button type="button" class="btn btn-light">Bearbeiten</button>
-            <button type="button" class="btn btn-light">Ansicht</button>
-            <button type="button" class="btn btn-light">Einfügen</button>
-            <button type="button" class="btn btn-light">Format</button>
-            <button type="button" class="btn btn-light">Tools</button>
+            <md-menu-content class="menu-content">
+              <md-menu-item @click="">Rückgängig machen</md-menu-item>
+              <md-menu-item @click="">Wiederholen</md-menu-item>
+              <md-menu-item @click="">Auusschneiden</md-menu-item>
+              <md-menu-item @click="">Kopieren</md-menu-item>
+              <md-menu-item @click="">Einfügen</md-menu-item>
+              <md-menu-item @click="">Löschen</md-menu-item>
+              <md-menu-item @click="">Ales auswählen</md-menu-item>
+            </md-menu-content>
+          </md-menu>
+          <md-menu md-size="small" md-align-trigger >
+            <button md-menu-trigger type="button" class="btn btn-light">Ansicht</button>
+
+            <md-menu-content class="menu-content">
+              <md-menu-item @click=""></md-menu-item>
+            </md-menu-content>
+          </md-menu>
+          <md-menu md-size="small" md-align-trigger >
+            <button md-menu-trigger type="button" class="btn btn-light">Einfügen</button>
+
+            <md-menu-content class="menu-content">
+              <md-menu-item @click=""></md-menu-item>
+            </md-menu-content>
+          </md-menu>
+          <md-menu md-size="small" md-align-trigger >
+            <button md-menu-trigger type="button" class="btn btn-light">Format</button>
+
+            <md-menu-content class="menu-content">
+              <md-menu-item @click=""></md-menu-item>
+            </md-menu-content>
+          </md-menu>
+          <md-menu md-size="small" md-align-trigger >
+            <button md-menu-trigger type="button" class="btn btn-light">Tools</button>
+
+            <md-menu-content class="menu-content">
+              <md-menu-item @click=""></md-menu-item>
+            </md-menu-content>
+          </md-menu>
           </div>
-        </md-menu><br>
+        <br>
         <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
-          <button type="button" class="btn btn-light"> <img class="fktstripImg" src="../assets/baseline-format_bold-24px.svg" /> </button>
+          <button type="button" class="btn btn-light"> <img class="fktstripImg" src="../assets/baseline-undo-24px.svg" /> </button>
+          <button type="button" class="btn btn-light"> <img class="fktstripImg" src="../assets/baseline-redo-24px.svg" /> </button>
+          <button type="button" class="btn btn-light"> <img class="fktstripImg" src="../assets/baseline-print-24px.svg" /> </button>
+          <button type="button" class="btn btn-light"> <img class="fktstripImg" src="../assets/baseline-format_paint-24px.svg" /> </button>
+        </div>
+        <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
+          <button type="button" class="btn btn-light" @click=""> <img class="fktstripImg" src="../assets/baseline-format_bold-24px.svg" /> </button>
           <button type="button" class="btn btn-light"> <img class="fktstripImg" src="../assets/baseline-format_italic-24px.svg" /> </button>
           <button type="button" class="btn btn-light"> <img class="fktstripImg" src="../assets/baseline-format_underlined-24px.svg" /> </button>
         </div>
@@ -43,17 +85,43 @@
         <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
           <button type="button" class="btn btn-light"> <img class="fktstripImg" src="../assets/baseline-format_list_numbered-24px.svg" /> </button>
           <button type="button" class="btn btn-light"> <img class="fktstripImg" src="../assets/baseline-format_list_bulleted-24px.svg" /> </button>
+          <button type="button" class="btn btn-light"> <img class="fktstripImg" src="../assets/baseline-format_indent_decrease-24px.svg" /> </button>
+          <button type="button" class="btn btn-light"> <img class="fktstripImg" src="../assets/baseline-format_indent_increase-24px.svg" /> </button>
         </div>
         <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
           <button type="button" class="btn btn-light"> <img class="fktstripImg" src="../assets/baseline-format_clear-24px.svg" /> </button>
         </div>
+        <!--
+        <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
+          <div class="md-layout-item">
+            <md-field class="">
+              <md-select v-model="font" name="font" id="font" placeholder="Font">
+                <md-option value="arial">Arial</md-option>
+                <md-option value="calibri">Calibri</md-option>
+                <md-option value="cambria">Cambria</md-option>
+                <md-option value="comic-sans">Comic Sans</md-option>
+                <md-option value="consolas">Consolas</md-option>
+                <md-option value="courier">Courier</md-option>
+                <md-option value="droid-sans">Droid Sans</md-option>
+                <md-option value="georgia">Georgia</md-option>
+                <md-option value="helvetica">Helvetica</md-option>
+                <md-option value="impact">Impact</md-option>
+                <md-option value="roboto">Roboto</md-option>
+                <md-option value="segoe-ui">Segoe UI</md-option>
+                <md-option value="times-new-roman">Times New Roman</md-option>
+                <md-option value="ubuntu">Ubuntu</md-option>
+                <md-option value="verdana">Verdana</md-option>
+              </md-select>
+            </md-field>
+          </div>
+        </div>
+        -->
       </div>
-      <div class="col-md-4 order-md-2 order-1">
-        <md-button style="margin-top: 1rem; float: right" type="button" onClick="history.go(-1);return true;" class="md-default md-raised md-dense">zurück</md-button>
-        <md-button style="margin-top: 1rem; float: right" type="button" @click="showDialogRename = true" class="md-default md-raised md-dense">umbenennen</md-button>
+      <div class="col-md-3 order-md-2 order-1">
+        <md-button style="float: right" type="button" to="/" class="md-default md-raised md-dense">zurück</md-button>
+        <md-button style="float: right" type="button" @click="showDialogRename = true" class="md-default md-raised md-dense">umbenennen</md-button>
       </div>
     </div>
-
 
     <md-dialog class="dialog" :md-active.sync="showDialogRename">
       <md-dialog-title>Neuen Namen eingeben</md-dialog-title>
@@ -70,9 +138,9 @@
       </md-dialog-actions>
     </md-dialog>
 
-
     <h1 style="margin-top: 3rem;">{{ doc.title }}</h1> {{ rename }}     <b-alert class="saving" :show="saveAlert" variant="info">speichert...</b-alert>
     <div style="outline:none" contenteditable="true"
+       id="paper"
        class="my-3 bg-white rounded shadow-lg paper"
        @input="onDivInput($event, doc)"
        v-html="body" :disabled="1">
@@ -131,7 +199,6 @@
   }
 </script>
 
-
 <style>
   main, main{
     height: 100%;
@@ -147,13 +214,17 @@
     top: -55px;
   }
 
+  .container {
+    padding-top: 0.5rem;
+  }
+
   .paper {
     min-height:1000px;
-    padding: 100px !important;
+    padding: 110px !important;
   }
 
   .btn-group {
-    margin: 0.3rem 0.3rem 0 0;
+    margin: 6px 0.3rem 0rem 0;
   }
 
   .dialog {
@@ -172,6 +243,7 @@
 
   .md-list-item-container {
     font-size: 0.875rem;
+    padding: 0 0.5rem 0 1.7rem;
   }
 
   .md-list-item-content {
@@ -181,4 +253,9 @@
   .menu-content {
     max-height: 1000rem;
   }
+
+  .md-layout-item {
+    width: 8rem;
+  }
+
 </style>
