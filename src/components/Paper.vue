c6febc3ee3f3e7ad2c97d454b91cda2e3e9c17ec
Samuel Werner <hallo@samuelwerner.de>
2018-12-22 21:18:21 +0100

    Heroku Test

--- src/components/Paper.vue
+++ src/components/Paper.vue
@@ -171,14 +171,16 @@
                     let diff = (differenceNew.result[i])
                     if (!diff) return
                     if (diff.EndDeletePosition - diff.StartInsertPosition > 0) { // Delete
-                      console.log('delete merge')
                       if (newValue.length < currentValue.length) { // nicht löschen wenn Inhalt hinzugefügt wurde
+                        console.log('delete merge')
                         distanceDiff += (result.substr(diff.StartInsertPosition, diff.EndDeletePosition)).length
-                        result = result.substr(0, diff.StartInsertPosition) + diff.newData + result.substr(diff.EndDeletePosition)
+                        result = result.substr(0, diff.StartInsertPosition) + result.substr(diff.EndDeletePosition)
+                        console.log('result delete merge' + result)
                       }
                     } else { // Insert
                       console.log('insert Merge')
                       result = result.substr(0, diff.StartInsertPosition) + diff.newData + result.substr(diff.StartInsertPosition)
+                      console.log('insert merge result: ' + result)
                       distanceDiff += diff.newData.length
                     }
                     that.$nextTick(() => {
