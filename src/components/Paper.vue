6abe053bc11340c39667280fc95853b138f36a0f
Christopher Sang <christopher.sang@iem.thm.de>
2019-01-02 23:44:19 +0100

    Paper scroll top before create

--- src/components/Paper.vue
+++ src/components/Paper.vue
@@ -108,6 +108,10 @@
         savedSelection: null
       }
     },
+    beforeCreate () {
+      document.body.scrollTop = 0 // For Safari
+      document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
+    },
     async created () {
       // Beim Aufruf der Seite das Dokument aufbauen
       let newB = document.createElement('div')
